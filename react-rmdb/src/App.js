9a27a8d2f10cf47edb1d817122064aeedd699907
SIRU TAN <siru0915@gmail.com>
2022-09-28 15:10:14 -0400

    timestamp:1:25:45

--- react-rmdb/src/App.js
+++ react-rmdb/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+//1:21:56
+import Header from './components/Header';
+
+import {GlobalStyle} from './GlobalStyles';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       Start here.
+      <GlobalStyle />
     </div>
   );
 }
