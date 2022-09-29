44a804a445838b0d9f132bde8fe8e390792d4e0b
SIRU TAN <siru0915@gmail.com>
2022-09-29 15:09:55 -0400

    timestamp:1:52:45

--- react-rmdb/src/components/Home.js
+++ react-rmdb/src/components/Home.js
@@ -4,6 +4,7 @@ import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
 
 import NoImages from '../images/no_image.jpg';
 
+import api from '../API';
 
 
 
@@ -12,6 +13,36 @@ const Home = () => {
     const [state, setState] = useState();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+
+    const fetchMovies = async (page, searchTerm = '') => {
+        try{
+            setError(false);
+            setLoading(true);
+
+            const movies = await api.fetchMovies(searchTerm,page);
+
+            setState(prev =>({
+                ...movies,
+                results:
+                page > 1 ? [...prev.results,...movies.results] : [...movies.results]
+            }))
+
+        }catch(error){
+            setError(true);
+        }
+        setLoading(false)
+    }
+
+    
+    //initial render
+    useEffect(()=>{
+        fetchMovies(1)
+    },[])
+    //array: dependencies for when the useEffect is triggered
+    //when empty it will only run once 
+    console.log(state)
+
+
 
     return<div>home page</div>
 }
